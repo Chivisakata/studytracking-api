c91f6d5232f78559fc2381aa5496a9f929b31183
Vĩ Cao <vifreefire@gmail.com>
2026-06-03 12:34:58 +0700

    create new repo

---
+++ studytracking-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using studytracking_api.Models.Entities;
+
+namespace studytracking_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ studytracking-api/Models/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace studytracking_api.Models.Entities
+{
+    public class User
+    {
+        public Guid Id { get ; set; } = Guid.NewGuid();
+        [Required]
+        public required string Username { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public required string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public UserRole Role { get; set; } = UserRole.User;
+
+    }
+
+    public enum UserRole
+    {
+            Admin,
+            User
+    }
+}
